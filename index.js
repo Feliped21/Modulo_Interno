34d1a1c7ab630e9b462a2176fef06340a31cbc68
Feliped23 <83152743+Feliped21@users.noreply.github.com>
2024-04-22 16:12:28 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// main.js
+
+// importando o módulo
+
+const meuModulo = require('./modulo');
+
+// utilizando funções do módulo
+
+console.log(meuModulo.soma(10, 7));
+console.log(meuModulo.subtração(10, 7));
---
+++ modulo.js
@@ -0,0 +1,11 @@
+// math.js
+
+// Função de soma
+exports.soma = function(a, b) {
+    return a + b;
+};
+
+// Função de subtração
+exports.subtração = function(a, b) {
+    return a - b;
+}
